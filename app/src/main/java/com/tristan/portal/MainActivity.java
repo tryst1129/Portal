19f06e014691c2193dea174b59be1b49ad573eb9
Tristan <tristanlarson@gmail.com>
2023-10-14 23:59:31 -0500

    v2
    v2 babeyy

--- app/src/main/java/com/tristan/portal/MainActivity.java
+++ app/src/main/java/com/tristan/portal/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,11 +15,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Window window = this.getWindow();
+        window.setStatusBarColor(getColor(R.color.transparent));
     }
 
     public void onClick (View view) {
 
+        Window window;
+        if (findViewById(R.id.good) == view) {
+            setContentView(R.layout.good_apps);
+            window = this.getWindow();
+            window.setStatusBarColor(getColor(R.color.green));
+            return;
+        }
+        if (findViewById(R.id.bad) == view) {
+            setContentView(R.layout.bad_apps);
+            window = this.getWindow();
+            window.setStatusBarColor(getColor(R.color.blue));
+            return;
+        }
+        if (findViewById(R.id.ugly) == view) {
+            setContentView(R.layout.ugly_apps);
+            window = this.getWindow();
+            window.setStatusBarColor(getColor(R.color.red));
+            return;
+        }
+
+
+        if (findViewById(R.id.backG) == view) {
+            setContentView(R.layout.activity_main);
+            return;
+        }
+        if (findViewById(R.id.backB) == view) {
+            setContentView(R.layout.activity_main);
+            return;
+        }
+        if (findViewById(R.id.backU) == view) {
+            setContentView(R.layout.activity_main);
+            return;
+        }
+
+
+
+
         String url = "https://google.com";
+
+
 
 
         if (findViewById(R.id.viMusic) == view) {
@@ -45,6 +87,70 @@ public class MainActivity extends AppCompatActivity {
         if (findViewById(R.id.ladb) == view) {
             url = "https://play.google.com/store/apps/details?id=com.draco.ladb&pcampaignid=web_share";
         }
+
+
+
+
+        if (findViewById(R.id.chase) == view) {
+            url = "https://play.google.com/store/apps/details?id=com.chase.sig.android&pcampaignid=web_share";
+        }
+        if (findViewById(R.id.one) == view) {
+            url = "https://play.google.com/store/apps/details?id=com.google.android.apps.subscriptions.red&pcampaignid=web_share";
+        }
+        if (findViewById(R.id.drive) == view) {
+            url = "https://play.google.com/store/apps/details?id=com.google.android.apps.docs&pcampaignid=web_share";
+        }
+        if (findViewById(R.id.files) == view) {
+            url = "https://play.google.com/store/apps/details?id=com.google.android.apps.nbu.files&pcampaignid=web_share";
+        }
+        if (findViewById(R.id.keyboard) == view) {
+            url = "https://play.google.com/store/apps/details?id=com.google.android.inputmethod.latin&pcampaignid=web_share";
+        }
+        if (findViewById(R.id.sengled) == view) {
+            url = "https://play.google.com/store/apps/details?id=com.sengled.life2&pcampaignid=web_share";
+        }
+        if (findViewById(R.id.goodreads) == view) {
+            url = "https://play.google.com/store/apps/details?id=com.goodreads&pcampaignid=web_share";
+        }
+        if (findViewById(R.id.books) == view) {
+            url = "https://play.google.com/store/apps/details?id=com.google.android.apps.books&pcampaignid=web_share";
+        }
+        if (findViewById(R.id.messages) == view) {
+            url = "https://play.google.com/store/apps/details?id=com.google.android.apps.messaging&pcampaignid=web_share";
+        }
+        if (findViewById(R.id.voice) == view) {
+            url = "https://play.google.com/store/apps/details?id=com.google.android.apps.googlevoice&pcampaignid=web_share";
+        }
+        if (findViewById(R.id.clock) == view) {
+            url = "https://play.google.com/store/apps/details?id=com.google.android.deskclock&pcampaignid=web_share";
+        }
+        if (findViewById(R.id.contacts) == view) {
+            url = "https://play.google.com/store/apps/details?id=com.google.android.contacts&pcampaignid=web_share";
+        }
+        if (findViewById(R.id.calendar) == view) {
+            url = "https://play.google.com/store/apps/details?id=com.google.android.calendar&pcampaignid=web_share";
+        }
+        if (findViewById(R.id.sheets) == view) {
+            url = "https://play.google.com/store/apps/details?id=com.google.android.apps.docs.editors.sheets&pcampaignid=web_share";
+        }
+
+
+
+        if (findViewById(R.id.infiniteCampus) == view) {
+            url = "https://play.google.com/store/apps/details?id=com.infinitecampus.student.campusportalhybrid&pcampaignid=web_share";
+        }
+        if (findViewById(R.id.schoology) == view) {
+            url = "https://play.google.com/store/apps/details?id=com.schoology.app&hl=en_ZA";
+        }
+        if (findViewById(R.id.hotSchedules) == view) {
+            url = "https://play.google.com/store/apps/details?id=com.tdr3.hs.android&pcampaignid=web_share";
+        }
+        if (findViewById(R.id.life360) == view) {
+            url = "https://play.google.com/store/apps/details?id=com.life360.android.safetymapd&pcampaignid=web_share";
+        }
+
+
+
 
 
 
